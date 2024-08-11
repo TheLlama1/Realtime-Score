fdefa3633695b5f9bc7d0d2518ccdd425f0789dc
TheLlama1 <martingugov20a@gmail.com>
2024-08-11 22:20:38 +0300

    navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -21,12 +21,19 @@ export default function Navbar() {
         <NavigationMenuList>
           <NavigationMenuItem>
             <div className="text-2xl ml-10">Realtime Score</div>
-            <NavigationMenuContent></NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <div className="text-center text-xl text-gray-500 border-2 rounded-lg border-gray-700">
               Search
             </div>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <button
+              type="button"
+              className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+            >
+              Sign in
+            </button>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
